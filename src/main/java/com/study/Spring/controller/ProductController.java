a36c6e4c13ba1f1bae60ec4dd4fcadd2e90a1315
Boseong@0902 <alskdj7879@gmail.com>
2025-07-06 18:13:05 +0900

    w#1-s#1

---
+++ src/main/java/com/study/Spring/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.study.Spring.controller;
+
+import com.study.Spring.entity.Product;
+import com.study.Spring.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productService.create(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.update(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        productService.delete(id);
+    }
+}
