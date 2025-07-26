bf3bee419723a317f363fcf22ee4976df8945b87
Boseong@0902 <alskdj7879@gmail.com>
2025-07-27 01:17:53 +0900

    productRepository수정

--- src/main/java/com/study/Spring/repository/ProductRepository.java
+++ src/main/java/com/study/Spring/repository/ProductRepository.java
@@ -45,6 +45,9 @@ package com.study.Spring.repository;
 
 import com.study.Spring.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findAll(Pageable pageable);
 }
